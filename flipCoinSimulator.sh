714005c1ccd6b9df0ba58c5d3795443a3283ca10
1301mohit <mohitkr1301@gmail.com>
2019-11-12 17:30:37 +0530

    calculate percentage of head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -x
 
-checkRandom=$((RANDOM%2))
-if [ $checkRandom -eq 0 ]
-then
-	echo head
-else
-	echo tail
-fi
+declare -A singlet
+head=0
+tail=0
+for(( i=0; i<10; i++ ))
+do
+	checkRandom=$((RANDOM%2))
+	if [ $checkRandom -eq 0 ]
+	then
+		singlet[H]=$((++head))
+	else
+		singlet[T]=$((++tail))
+	fi
+done
+echo ${!singlet[@]}
+echo ${singlet[@]}
+percentageOfHead=$(( $((${singlet[H]} * 100)) / $(( ${singlet[H]} + ${singlet[T]} )) ))
+percentageOfTail=$(( $((${singlet[T]} * 100)) / $(( ${singlet[H]} + ${singlet[T]} )) ))
