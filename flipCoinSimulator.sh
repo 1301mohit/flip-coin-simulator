340a8ae18a8ac047c9609fd768d24f1a7ca54d3d
1301mohit <mohitkr1301@gmail.com>
2019-11-12 19:05:08 +0530

    calaulate percentage of doublet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,19 +1,37 @@
 #!/bin/bash -x
 
 declare -A singlet
-head=0
-tail=0
-for(( i=0; i<10; i++ ))
+hh=0
+ht=0
+th=0
+tt=0
+numberOfTimesFlipCoin=10
+singlet[HH]=$hh
+singlet[HT]=$ht
+singlet[TH]=$th
+singlet[TT]=$tt
+for(( i=0; i<$numberOfTimesFlipCoin; i++ ))
 do
+	checkRandom1=$((RANDOM%2))
 	checkRandom=$((RANDOM%2))
-	if [ $checkRandom -eq 0 ]
+	if [ $checkRandom -eq 0 -a $checkRandom1 -eq 0 ]
 	then
-		singlet[H]=$((++head))
-	else
-		singlet[T]=$((++tail))
+		singlet[HH]=$((++hh))
+	elif [ $checkRandom -eq 0 -a $checkRandom1 -eq 1 ]
+	then
+		singlet[HT]=$((++ht))
+	elif [ $checkRandom -eq 1 -a $checkRandom1 -eq 0 ]
+	then
+		singlet[TH]=$((++th))
+	elif [ $checkRandom -eq 1 -a $checkRandom1 -eq 1 ]
+	then
+		singlet[TT]=$((++tt))
 	fi
 done
 echo ${!singlet[@]}
 echo ${singlet[@]}
-percentageOfHead=$(( $((${singlet[H]} * 100)) / $(( ${singlet[H]} + ${singlet[T]} )) ))
-percentageOfTail=$(( $((${singlet[T]} * 100)) / $(( ${singlet[H]} + ${singlet[T]} )) ))
+total=$(( ${singlet[HH]} + ${singlet[HT]} + ${singlet[TH]} + ${singlet[TT]} ))
+percentageOfHeadHead=$(( $((${singlet[HH]} * 100)) / $total ))
+percentageOfHeadTail=$(( $((${singlet[HT]} * 100)) / $total ))
+percentageOfTailHead=$(( $((${singlet[TH]} * 100)) / $total ))
+percentageOfTailTail=$(( $((${singlet[TT]} * 100)) / $total ))
